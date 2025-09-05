e1dfea1e6a4b1976185aa57596145fe43b113292
tor-scarf <mehdiking503@gmail.com>
2025-09-06 02:24:12 +0330

    Delete app/components directory

--- app/components/Header.js
+++
@@ -1,12 +0,0 @@
-use client;
-
-export default function Header() {
-  return (
-    <header className="w-full text-center mb-10">
-      <h1 className="text-4xl font-extrabold text-yellow-400 drop-shadow-lg mb-2">
-        🛍️ فروشگاه Tor.scarf
-      </h1>
-      <p className="text-lg text-yellow-200">بهترین شال و روسری‌ها برای شما</p>
-    </header>
-  );
-}
