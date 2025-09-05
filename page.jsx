441b49fd4fccb2eda5f5305458205eab34a222a3
tor-scarf <mehdiking503@gmail.com>
2025-09-05 23:16:47 +0330

    Delete page.jsx

--- page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-black text-gold">
-      <h1 className="text-4xl font-bold">Tor.scarf فروشگاه</h1>
-      <p className="mt-4">به فروشگاه آنلاین ما خوش آمدید 🖤✨</p>
-    </main>
-  );
-}
