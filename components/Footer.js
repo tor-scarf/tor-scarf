a85588f1a0b235cb094a5c74c6ff778b61adcfa1
tor-scarf <mehdiking503@gmail.com>
2025-09-06 02:18:47 +0330

    Delete components directory

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-6 mt-10">
-      <div className="container mx-auto text-center">
-        <p>© 2025 Tor.scarf - همه حقوق محفوظ است.</p>
-      </div>
-    </footer>
-  )
-}
