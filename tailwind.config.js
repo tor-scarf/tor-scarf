1fbb5931b25fbaf7b3dbc2f2381c15f7b0c6e536
tor-scarf <mehdiking503@gmail.com>
2025-09-06 20:15:32 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#FFD700',
+      },
+    },
+  },
+  plugins: [],
+}
