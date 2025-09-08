3df5e172e63a873e0e6d62129622b806db665367
tor-scarf <mehdiking503@gmail.com>
2025-09-09 02:15:32 +0330

    Delete app directory

--- app/dashboard/user/page.js
+++
@@ -1,8 +0,0 @@
-export default function UserDashboard() {
-  return (
-    <div className="p-6">
-      <h1 className="text-xl font-bold">پنل کاربری</h1>
-      <p>اینجا سفارش‌ها و پروفایل شما نمایش داده می‌شود.</p>
-    </div>
-  );
-}
